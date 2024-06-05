6c2b8701e829b562df8fec75c6a1d5e8f86c35dd
meargteame <mearegteame99995555@gmail.com>
2024-06-05 10:18:03 -1200

    task12done

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,4 +1,4 @@
-#include"binary_trees.h"
+#include "binary_trees.h"
 
 /**
  * binary_tree_leaves - Counts the leaves in a binary tree
@@ -6,14 +6,13 @@
  *
  * Return: Number of leaves in the tree, or 0 if tree is NULL
  */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return (0);
 
-size_t binary_tree_leaves(const binary_tree_t *tree){
+    if (tree->left == NULL && tree->right == NULL)
+        return (1);
 
-    if(tree==NULL){
-        return 0;
-    }
-    if(tree->left ==NULL && tree->right ==NULL){
-    return (1);
-    }
-      return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
-    }
+    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
