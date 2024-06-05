4ee3c5e8fab80472e549a678e889aec83c20e159
meargteame <mearegteame99995555@gmail.com>
2024-06-05 04:04:10 -1200

    task11done

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - function that return the size of a tree
+ * @tree: tree to check
+ * Return: size of tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		size =  binary_tree_size(tree->left) + binary_tree_size(tree->right)+ 1;
+	}
+	return (size);
+}
