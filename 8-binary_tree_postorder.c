ab229cd89ff22f56bf77658393c17125d85aa77c
meargteame <mearegteame99995555@gmail.com>
2024-06-05 03:38:14 -1200

    task8done

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,12 +1,11 @@
-#include "binary_trees.h"
 
+#include "binary_trees.h"
 /**
- * binary_tree_preorder - print elements of tree using pre-order traversal
+ * binary_tree_postorder - print elements of tree using post-order traversal
  * @tree: tree to go through
  * @func: function to use
  * Return: Nothing
  */
-
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
@@ -17,6 +16,6 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	{
 		binary_tree_postorder(tree->left, func);
 		binary_tree_postorder(tree->right, func);
-		func(tree->n);
 	}
+	func(tree->n);
 }
