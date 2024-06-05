8aad465df96bd5839fb01ba49313e3bb26901bdf
meargteame <mearegteame99995555@gmail.com>
2024-06-05 03:49:48 -1200

    task9done

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,13 +6,22 @@
  * Return: the height
  */
 
-int max(int a, int b) {
-    return (a > b) ? a : b;
-}
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l = 0;
+	size_t r = 0;
 
-size_t binary_tree_height(const binary_tree_t *tree) {
-    if (tree == NULL) {
-        return (0);
-    }
-    return 1 + max(binary_tree_height(tree->left), binary_tree_height(tree->right));
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree)
+		{
+			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+		return ((l > r) ? l : r);
+	}
 }
