0433b346c69e4e1694a541278ccff35d3a3e313f
meargteame <mearegteame99995555@gmail.com>
2024-06-05 03:28:08 -1200

    task6done

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,4 +1,4 @@
-#include"binary_trees.h"
+#include "binary_trees.h"
 
 /**
  * binary_tree_preorder - print elements of tree using pre-order traversal
@@ -7,14 +7,16 @@
  * Return: Nothing
  */
 
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int)){
-  if (tree ==NULL ||func ==NULL) return;
-
-else{
-  func(tree->n);
-  binary_tree_preorder(tree->left,func);
-  binary_tree_preorder(tree->right,func);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	else
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
 }
-
-}
-
