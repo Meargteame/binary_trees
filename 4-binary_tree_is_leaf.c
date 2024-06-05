c12d48cf7c6e40a18078385defead3390da16577
meargteame <mearegteame99995555@gmail.com>
2024-06-05 02:50:27 -1200

    task4done

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,11 +7,16 @@
  * Return: 1 if it is a leaf or 0 if it is not
  */
 int binary_tree_is_leaf(const binary_tree_t *node){
-  if (node ==NULL) return 0;
-  if(node->left ==NULL && node->right ==NULL){
+
+  if (node ==NULL) {
+    return (0);
+  }
+  if(node->left ==NULL && node->right ==NULL)
+  {
     return (1);
   }
   else{
     return (0);
   }
 }
+
