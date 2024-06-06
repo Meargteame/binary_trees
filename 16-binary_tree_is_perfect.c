f145a68348eb3df16c5abf905a183a7204e7fbfd
meargteame <mearegteame99995555@gmail.com>
2024-06-05 17:39:46 -1200

    task16update

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,10 +1,18 @@
 #include"binary_trees.h"
 
 int binary_tree_is_perfect(const binary_tree_t *tree){
-  if(tree ==NULL) return (0);
+  if(tree ==NULL) {
+    return (0);
+  }
+
   if(tree->left && tree->right) {
+    
     binary_tree_is_perfect(tree->left);
     binary_tree_is_perfect(tree->right);
   }
   return (1);
 }
+
+
+// perfect binary tree 
+// all leaves have the same depth or same level 
