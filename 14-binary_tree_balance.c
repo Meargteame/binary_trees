b24e7888210a0fa52a63712250f3caf4c5617841
meargteame <mearegteame99995555@gmail.com>
2024-06-05 16:01:06 -1200

    task15done

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,7 +16,6 @@ int binary_tree_balance(const binary_tree_t *tree)
         
     int left_height = (int) binary_tree_height(tree->left);
     int right_height = (int)binary_tree_height(tree->right);
-    
     total = left_height - right_height;
     return (total);
     }
